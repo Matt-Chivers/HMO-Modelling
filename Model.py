42ff8664a92efb28453062c8e489abc7b011bf09
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-09-26 15:59:40 +0100

    update

--- Model.py
+++ Model.py
@@ -2,4 +2,4 @@
 # Depending on what parameters we have, depends on what method we will call in order to calculate the necessary calculation. 
 # This means that the code can function fine without having to have to change the calculation if we don't have certain parameters.
 
-
+print("Hello")
