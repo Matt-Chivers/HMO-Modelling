7b93c4dc211174e0da0cd659e8a7e965a1ebd2af
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-09-26 17:15:38 +0100

    Update GeneralModel.py

--- GeneralModel.py
+++ GeneralModel.py
@@ -1,5 +1,24 @@
-# Hardcode lots of different parameters. If there is no value for a parameter, let it equal 0.
-# Depending on what parameters we have, depends on what method we will call in order to calculate the necessary calculation. 
-# This means that the code can function fine without having to have to change the calculation if we don't have certain parameters.
+# This method takes all the parameters and outputs the ones wich are not equal to zero.
+x1 = 1
+x2 = 2
+x3 = 0
+x4 = 0
+x5 = 6
 
+def check_for_non_zero_parameters(x1, x2, x3, x4, x5):
+    parameters = [x1, x2, x3, x4, x5]
+    parameters_in_use = []
+
+    for param in parameters:
+        if param != 0:
+            parameters_in_use.append(param)
+
+    return parameters_in_use
+
+non_zero_parameters = check_for_non_zero_parameters(x1, x2, x3, x4, x5)
+
+# Install package
+
+
+    
 
