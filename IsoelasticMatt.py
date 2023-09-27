133e3cce468f8b147e2ece41ec331a1411bb0da1
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-09-27 23:28:18 +0100

    update

--- IsoelasticMatt.py
+++ IsoelasticMatt.py
@@ -16,27 +16,29 @@ def isoelastic_demand(p):
 # Calculate demand quantity for the given price
 q_demand = isoelastic_demand(price)
 
-# Define the vertical supply function
+# Generate a range of p values (price values)
+p_values = np.linspace(500, 1000, 100)
+
+# Calculate corresponding q values (quantity values) using the isoelastic demand function
+q_demand_values = isoelastic_demand(p_values)
+
+# Define the vertical supply function (supply curve coming from x-axis)
 def vertical_supply(p):
     return quantity
 
 # Calculate supply quantity for the given price
 q_supply = vertical_supply(price)
 
-# Generate a range of p values
-p_values = np.linspace(500, 1000, 100)
-
-# Calculate corresponding q values using the isoelastic demand and vertical supply functions
-q_demand_values = isoelastic_demand(p_values)
+# Generate a vertical supply curve (constant quantity)
 q_supply_values = [vertical_supply(p) for p in p_values]
 
 # Plot the isoelastic demand and vertical supply curves
-plt.plot(p_values, q_demand_values, label='Isoelastic Demand Curve', color='r')
-plt.axvline(x=price, color='g', linestyle='--', label='Supply Curve')  # Vertical supply line
-plt.scatter(price, q_demand, color='b', label=f'Point of equilibrium: (p={price}, q={q_demand})')
-plt.xlabel('Price £ (p)')
-plt.ylabel('Quantity (q)')
-plt.title('Supply and Isoelastic Demand')
+plt.plot(q_demand_values, p_values, label='Isoelastic Demand Curve', color='r')
+plt.axvline(x=quantity, color='g', linestyle='--', label=f'Supply Curve (q={quantity})')  # Vertical supply line
+plt.scatter(quantity, price, color='b', label=f'Point of equilibrium: (q={quantity}, p={price})')
+plt.xlabel('Quantity (q)')
+plt.ylabel('Price £ (p)')
+plt.title('Isoelastic Demand and Vertical Supply')
 plt.legend()
 plt.grid(True)
 plt.show()
