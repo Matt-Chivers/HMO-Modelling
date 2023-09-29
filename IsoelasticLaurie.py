84873d9fe9b47df79889eb55bda5ed68cbd01a6f
LDewar <145987273+LDewar@users.noreply.github.com>
2023-09-29 10:56:05 +0100

    Update IsoelasticLaurie.py

--- IsoelasticLaurie.py
+++ IsoelasticLaurie.py
@@ -11,7 +11,7 @@ params = {
 
 target_parameter = ""
 
-# Your code for calculating the missing parameter
+# Code for calculating the missing parameter
 def calculate_missing_parameter(params, target_parameter):
      for key in params:
         if params[key] == 0:
@@ -21,45 +21,9 @@ def calculate_missing_parameter(params, target_parameter):
             elif target_parameter == 'price':
                 params[target_parameter] = (params['quant'] / params['a']) ** (-1 / params['e'])
             elif target_parameter == 'e':
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-                params[target_parameter] = -math.log(params['quant'] / params['a'], params['price'])
-            elif target_parameter == 'a':
-                params[target_parameter] = params['quant'] / (params['price'] ** -params['e'] )    
-            return params[target_parameter]
-
-print(calculate_missing_parameter(params, target_parameter)) 
-# Calculate the missing parameter based on the specified relationship quant = a * price^(-e)
-
-
-
-=======
-                params[target_parameter] = -math.log(params['quant'], params['price'])
-=======
                 params[target_parameter] = -math.log(params['quant']/ params['a'], params['price'])
->>>>>>> Stashed changes
             elif target_parameter == 'a':
                 params[target_parameter] = params['quant'] / (params['price'] ** -params['e'] )    
             return params[target_parameter]
             
-print(calculate_missing_parameter(params, target_parameter)) 
-# Calculate the missing parameter based on the specified relationship quant = a * price^(-e)
-
-    
-    
->>>>>>> 3ef207a7ed19f5e0120212559cf3da3b2757434f
-
-
-
-# class TestCalculateMissingParameter(unittest.TestCase):
-
-#     def test_calculate_missing_a(self):
-#         result = calculate_missing_parameter({'quant': 6802, 'price': 700, 'e': .8, 'a': None}, 'a')
-#         self.assertAlmostEqual(result, 4.0, places=2)  # 'a' is calculated based on the relationship
-
-# if __name__ == '__main__':
-<<<<<<< HEAD
-#     unittest.main()
-=======
-#     unittest.main()
->>>>>>> 3ef207a7ed19f5e0120212559cf3da3b2757434f
+print(calculate_missing_parameter(params, target_parameter))
