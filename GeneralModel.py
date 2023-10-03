2ffe4908a827deb73200468678981391f5407131
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-10-03 10:11:51 +0100

    update

--- GeneralModel.py
+++ GeneralModel.py
@@ -6,14 +6,14 @@ import math
 params = {
     'quant': 6802,
     'price': 700,
-    'elas': 0.8,
+    'elas': '?',
     'x1': 1284443.7,
 }
 
 # This method takes all the parameters and outputs the ones which are not equal to zero.
 def calculate_missing_parameter(params, target_parameter):
     for key in params:
-        if params[key] == 0:
+        if params[key] == '?':
             target_parameter = key
             if target_parameter == 'quant':
                 params[target_parameter] = params['x1'] * params['price'] ** -params['elas']
@@ -25,9 +25,9 @@ def calculate_missing_parameter(params, target_parameter):
                 params[target_parameter] = params['quant'] / (params['price'] ** -params['elas'])
             return params[target_parameter]
 
-target_parameter = 'price'  # Replace with the parameter you want to calculate
+target_parameter = 'elas'  # Replace with the parameter you want to calculate
 calculate_missing_parameter(params, target_parameter)
-print(f'{target_parameter}: {params[target_parameter]}')
+print(f'{target_parameter}: {round(params[target_parameter], 2)}')
 
 # Solve equation for demand
 a = params['quant'] / (params['price'] ** -params['elas'])
--- IsoelasticLaurie.py
+++ IsoelasticLaurie.py
@@ -4,14 +4,14 @@ import math
 # Define a dictionary to hold the initial parameter values
 params = {
     'quant': 1,
-    'price': 0,
+    'price': 1111,
     'e': 1,
     'a': 3,
 }
 
 target_parameter = ""
 
-# Your code for calculating the missing parameter
+# Code for calculating the missing parameter
 def calculate_missing_parameter(params, target_parameter):
      for key in params:
         if params[key] == 0:
@@ -21,28 +21,9 @@ def calculate_missing_parameter(params, target_parameter):
             elif target_parameter == 'price':
                 params[target_parameter] = (params['quant'] / params['a']) ** (-1 / params['e'])
             elif target_parameter == 'e':
-                params[target_parameter] = -math.log(params['quant'] / params['a'], params['price'])
+                params[target_parameter] = -math.log(params['quant']/ params['a'], params['price'])
             elif target_parameter == 'a':
                 params[target_parameter] = params['quant'] / (params['price'] ** -params['e'] )    
             return params[target_parameter]
-
-print(calculate_missing_parameter(params, target_parameter)) 
-# Calculate the missing parameter based on the specified relationship quant = a * price^(-e)
-
-elif target_parameter == 'a':
-    params[target_parameter] = params['quant'] / (params['price'] ** -params['e'] )    
-return params[target_parameter]
             
-print(calculate_missing_parameter(params, target_parameter)) 
-# Calculate the missing parameter based on the specified relationship quant = a * price^(-e)
-
-
-
-# class TestCalculateMissingParameter(unittest.TestCase):
-
-#     def test_calculate_missing_a(self):
-#         result = calculate_missing_parameter({'quant': 6802, 'price': 700, 'e': .8, 'a': None}, 'a')
-#         self.assertAlmostEqual(result, 4.0, places=2)  # 'a' is calculated based on the relationship
-
-# if __name__ == '__main__':
-#     unittest.main()
+print(calculate_missing_parameter(params, target_parameter))
