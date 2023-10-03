cd4b2b40e2c341eb4dc19915be254d2cc12b28fc
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-10-03 12:55:38 +0100

    update

--- CounterfactualMatt.py
+++ CounterfactualMatt.py
@@ -10,38 +10,33 @@ params = {
     'x1': 1284443.7,
 }
 
-# This method takes all the parameters and outputs the ones which are not equal to zero.
+# This method takes all the parameters and calculates the missing one.
 def calculate_missing_parameter(params, target_parameter):
-    for key in params:
-        if params[key] == '?':
-            target_parameter = key
-            if target_parameter == 'quant':
-                params[target_parameter] = params['x1'] * params['price'] ** -params['elas']
-            elif target_parameter == 'price':
-                params[target_parameter] = (params['quant'] / params['x1']) ** (-1 / params['elas'])
-            elif target_parameter == 'elas':
-                params[target_parameter] = -math.log(params['quant'] / params['x1']) / math.log(params['price'])
-            elif target_parameter == 'x1':
-                params[target_parameter] = params['quant'] / (params['price'] ** -params['elas'])
-            return params[target_parameter]
+    if params[target_parameter] != '?':
+        return params[target_parameter]
+    elif target_parameter == 'quant':
+        params[target_parameter] = params['x1'] * params['price'] ** -params['elas']
+    elif target_parameter == 'price':
+        params[target_parameter] = (params['quant'] / params['x1']) ** (-1 / params['elas'])
+    elif target_parameter == 'elas':
+        params[target_parameter] = -math.log(params['quant'] / params['x1']) / math.log(params['price'])
+    elif target_parameter == 'x1':
+        params[target_parameter] = params['quant'] / (params['price'] ** -params['elas'])
+    return params[target_parameter]
 
 target_parameter = 'elas'  # Replace with the parameter you want to calculate
-calculate_missing_parameter(params, target_parameter)
-print(f'{target_parameter}: {round(params[target_parameter], 2)}')
+calculated_value = calculate_missing_parameter(params, target_parameter)
+print(f'{target_parameter}: {round(calculated_value, 2)}')
 
 # Define the isoelastic demand function
-def isoelastic_demand(params):
-    a = params['quant'] / (params['price'] ** -params['elas'])
-    return a * p ** (-elas), a
+def isoelastic_demand(p, a, elas):
+    q = a * p ** (-elas)
+    return q
 
 # Calculate demand quantity for the given price
-
-
-# Generate a range of p values (price values)
-p_values = np.linspace(500, 1000, 100)
-
-# Calculate corresponding q values (quantity values) using the isoelastic demand function
-q_demand_values = isoelastic_demand(p_values, a, params['elas'])
+a = params['quant'] / (params['price'] ** -params['elas'])
+p_values = np.linspace(0, 1000, 100)
+q_demand_values = [isoelastic_demand(p, a, params['elas']) for p in p_values]
 
 # Define the vertical supply function (supply curve coming from x-axis)
 def vertical_supply(p):
@@ -49,40 +44,58 @@ def vertical_supply(p):
 
 # Calculate supply quantity for the given price
 q_supply = vertical_supply(params['price'])
-
-# Generate a vertical supply curve (constant quantity)
 q_supply_values = [vertical_supply(p) for p in p_values]
 
 # Plot the isoelastic demand and vertical supply curves
+plt.figure(figsize=(12, 6))
+
+# Plot the first graph (original data)
+plt.subplot(1, 2, 1)
 plt.plot(q_demand_values, p_values, label='Isoelastic Demand Curve', color='r')
 plt.axvline(x=params['quant'], color='g', linestyle='--', label=f'Supply Curve')
 plt.scatter(params['quant'], params['price'], color='b', label=f'Point of equilibrium: (q={round(params["quant"])}, p={round(params["price"])})')
 plt.xlabel('Quantity (q)')
 plt.ylabel('Price £ (p)')
-plt.title('Isoelastic Demand and Supply Curves')
+plt.title('Original Data')
 plt.legend()
 plt.grid(True)
-plt.show()
 
-
-#Define Counterfactual method
+# Define Counterfactual method
 def counterfactual(params):
-    counterfactual_percent = input("Counterfactual: What percentage would you like " + counterfactual_parameter + "to be increase / decrease by? If you do not want to add a change, type 0: " )
+    counterfactual_parameter = input("Counterfactual: What parameter would you like to change? (quant, price, elas, x1): ")
+    counterfactual_percent = float(input("By what percentage would you like to change it? If you do not want to add a change, type 0: "))
 
-    if counterfactual_percent == 'quant':
-        params['quant'] = params['quant'] * (1 + (counterfactual_percent / 100))
-    elif counterfactual_percent == 'price': 
-        params['price'] = params['price'] * (1 + (counterfactual_percent / 100))
-    elif counterfactual_percent == 'elas':
-        params['elas'] = params['elas'] * (1 + (counterfactual_percent / 100))  
-    elif counterfactual_percent == 'x1':
-        params['x1'] = params['x1'] * (1 + (counterfactual_percent / 100))
-    return params 
+    if counterfactual_parameter == 'quant':
+        params['quant'] *= (1 + (counterfactual_percent / 100))
+        # Adjust price while keeping elasticity constant
+        params['price'] = (params['quant'] / params['x1']) ** (-1 / params['elas'])
+    elif counterfactual_parameter == 'price':
+        params['price'] *= (1 + (counterfactual_percent / 100))
+    elif counterfactual_parameter == 'elas':
+        params['elas'] *= (1 + (counterfactual_percent / 100))
+    elif counterfactual_parameter == 'x1':
+        params['x1'] *= (1 + (counterfactual_percent / 100))
+    return params
 
-def counterfactual_isoelastic(params):
-    isoelastic_demand(p, a, elas)
+# Apply counterfactual to the parameters
+params = counterfactual(params)
+print("Updated Parameters:", params)
 
-         
+# Calculate demand quantity for the given price with updated parameters
+a = params['quant'] / (params['price'] ** -params['elas'])
+p_values = np.linspace(0, 10000, 100)  # Updated range for p_values
+q_demand_values = [isoelastic_demand(p, a, params['elas']) for p in p_values]
 
+# Plot the second graph (data after counterfactual changes)
+plt.subplot(1, 2, 2)
+plt.plot(q_demand_values, p_values, label='Isoelastic Demand Curve', color='r')
+plt.axvline(x=params['quant'], color='g', linestyle='--', label=f'Supply Curve')
+plt.scatter(params['quant'], params['price'], color='b', label=f'Point of equilibrium: (q={round(params["quant"])}, p={round(params["price"])})')
+plt.xlabel('Quantity (q)')
+plt.ylabel('Price £ (p)')
+plt.title('Data after Counterfactual Changes')
+plt.legend()
+plt.grid(True)
 
-counterfactual_parameter = 'price' # Replace with parameter you want to add a counterfactual to
+plt.tight_layout()
+plt.show()
