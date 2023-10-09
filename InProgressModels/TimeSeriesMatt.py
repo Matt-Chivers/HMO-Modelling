b78c89f4f9c3d0bee941e81a5a1ff356dd13ee92
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-10-09 20:34:45 +0100

    update

--- InProgressModels/TimeSeriesMatt.py
+++ InProgressModels/TimeSeriesMatt.py
@@ -44,37 +44,36 @@ for academic_year, population in student_population.items():
 print(price_values)
 
 # Must have 5 data points in each array
-# Data set 1
-x1_data = np.array([2018, 2019, 2020, 2021, 2022])
-y1_data = np.array(price_values[:len(x1_data)])  # Use the first 5 elements of price_values for y1_data
+x_data = np.array([2018, 2019, 2020, 2021, 2022])
+y_data = np.array(price_values[:len(x_data)])  # Use the first 5 elements of price_values for y1_data
 
 # Define the model function
 def model_f(x, a, b, c):
     return a * (x - b) ** 2 + c
 
 # Create a custom range of x-values with no interval
-x1_custom = np.arange(min(x1_data), max(x1_data) + 1)
+x_custom = np.arange(min(x_data), max(x_data) + 1)
 
 # Fit the model to data set 1 with initial guesses and a different optimization method
 initial_guess = (1.0, 2019.0, 3000.0)  # Adjust these initial values as needed
-popt1, pcov1 = curve_fit(model_f, x1_data, y1_data, p0=initial_guess)
+popt1, pcov1 = curve_fit(model_f, x_data, y_data, p0=initial_guess)
 a_opt1, b_opt1, c_opt1 = popt1
-x1_model = np.linspace(min(x1_custom), max(x1_custom), 100)
-y1_model = model_f(x1_model, a_opt1, b_opt1, c_opt1)
+x_model = np.linspace(min(x_custom), max(x_custom), 100)
+y_model = model_f(x_model, a_opt1, b_opt1, c_opt1)
 
 # Create a subplot with 1 row and 1 column
 fig, axs = plt.subplots(1, 1, figsize=(8, 4))
 
 # Plot data set 1 on the subplot
-axs.scatter(x1_data, y1_data, label='Data Set 1')
-axs.plot(x1_model, y1_model, label='Model 1', color='r')
+axs.scatter(x_data, y_data, label='Data Set 1')
+axs.plot(x_model, y_model, label='Model 1', color='r')
 axs.set_title('Time-Series 1')
 axs.set_xlabel('Time')
 axs.set_ylabel('Point of Equilibrium')
 
 # Set custom x-axis ticks and labels for the subplot
-axs.set_xticks(x1_custom)
-axs.set_xticklabels(x1_custom)
+axs.set_xticks(x_custom)
+axs.set_xticklabels(x_custom)
 
 # Add a legend
 axs.legend()
