369e57cf2ee051d179d8d48696eb5f452f5a0993
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-10-09 20:54:45 +0100

    update

--- InProgressModels/TimeSeriesMatt.py
+++ InProgressModels/TimeSeriesMatt.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 # Define a dictionary to hold the initial parameter values
 params = {
     'elas': 0.67,
-    'x1': 3,
+    'x1': 54.27,
 }
 
 # Define the student population dictionary
@@ -14,7 +14,7 @@ student_population = {
     '2019/20': 9224,
     '2020/21': 10119,
     '2021/22': 10425,
-    '2022/23': 8619,  # Updated population for 2022/23
+    '2022/23': 10425,  # Updated population for 2022/23
 }
 
 # Define the student population dictionary
@@ -41,11 +41,9 @@ for academic_year, population in student_population.items():
     print(f"{academic_year}: {price}")
     print(f"{quant}")
 
-print(price_values)
-
 # Must have 5 data points in each array
 x_data = np.array([2018, 2019, 2020, 2021, 2022])
-y_data = np.array(price_values[:len(x_data)])  # Use the first 5 elements of price_values for y1_data
+y_data = np.array(price_values[:len(x_data)])  # Use the first 5 elements of price_values for y_data
 
 # Define the model function
 def model_f(x, a, b, c):
@@ -54,9 +52,9 @@ def model_f(x, a, b, c):
 # Create a custom range of x-values with no interval
 x_custom = np.arange(min(x_data), max(x_data) + 1)
 
-# Fit the model to data set 1 with initial guesses and a different optimization method
-initial_guess = (1.0, 2019.0, 3000.0)  # Adjust these initial values as needed
-popt1, pcov1 = curve_fit(model_f, x_data, y_data, p0=initial_guess)
+# Fit the model to data set 1 with adjusted initial guesses and a different optimization method (lm)
+initial_guess = (1.0, 2020.0, 4000.0)  # Adjust these initial values as needed
+popt1, pcov1 = curve_fit(model_f, x_data, y_data, p0=initial_guess, method='lm')
 a_opt1, b_opt1, c_opt1 = popt1
 x_model = np.linspace(min(x_custom), max(x_custom), 100)
 y_model = model_f(x_model, a_opt1, b_opt1, c_opt1)
@@ -67,16 +65,14 @@ fig, axs = plt.subplots(1, 1, figsize=(8, 4))
 # Plot data set 1 on the subplot
 axs.scatter(x_data, y_data, label='Data Set 1')
 axs.plot(x_model, y_model, label='Model 1', color='r')
-axs.set_title('Time-Series 1')
+axs.set_title('Time-Series Equilibrium Price')
 axs.set_xlabel('Time')
 axs.set_ylabel('Point of Equilibrium')
 
 # Set custom x-axis ticks and labels for the subplot
 axs.set_xticks(x_custom)
 axs.set_xticklabels(x_custom)
-
-# Add a legend
 axs.legend()
 
-# Show the plot
+# Display the plot
 plt.show()
