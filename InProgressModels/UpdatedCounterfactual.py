3bf079840c5bc9d3d7df9a320614a537fc2fe5a9
Matt-Chivers <145900578+Matt-Chivers@users.noreply.github.com>
2023-10-24 14:01:32 +0100

    update

--- InProgressModels/UpdatedCounterfactual.py
+++ InProgressModels/UpdatedCounterfactual.py
@@ -6,8 +6,8 @@ import math
 params = {
     'quant': 6802,
     'price': 744,
-    'elas': 1.4,
-    'x1': '?',
+    'elas': 0.67,
+    'x1': 1,
 }
 
 # This method takes all the parameters and calculates the missing one.
@@ -24,27 +24,25 @@ def calculate_missing_parameter(params, target_parameter):
         params[target_parameter] = params['quant'] / (params['price'] ** -params['elas'])
     return params[target_parameter]
 
-
 target_parameter = 'x1'  # Replace with the parameter you want to calculate
 calculated_value = calculate_missing_parameter(params, target_parameter)
 print(f'{target_parameter}: {round(calculated_value, 2)}')
 
 # Define the isoelastic demand function
-def isoelastic_demand(p, a, elas):
-    q = a * p ** (-elas)
+def isoelastic_demand(p, x1, a, elas):
+    q = (x1 * a) * p ** (-elas)
     return q
 
 # Calculate demand quantity for the given price
 a = params['quant'] / (params['price'] ** -params['elas'])
 p_values = np.linspace(0, 1000, 100)
-q_demand_values = [isoelastic_demand(p, a, params['elas']) for p in p_values]
+q_demand_values = [isoelastic_demand(p, params['x1'], a, params['elas']) for p in p_values]
 
 # Define the vertical supply function (supply curve coming from x-axis)
 def vertical_supply(p):
     return params['quant']
 
 # Calculate supply quantity for the given price
-q_supply = vertical_supply(params['price'])
 q_supply_values = [vertical_supply(p) for p in p_values]
 
 # Plot the isoelastic demand and vertical supply curves
@@ -85,8 +83,7 @@ print("Updated Parameters:", params)
 
 # Calculate demand quantity for the given price with updated parameters
 a = params['quant'] / (params['price'] ** -params['elas'])
-p_values = np.linspace(0, 1000, 100)  # Updated range for p_values
-q_demand_values = [isoelastic_demand(p, a, params['elas']) for p in p_values]
+q_demand_values = [isoelastic_demand(p, params['x1'], a, params['elas']) for p in p_values]
 
 # Plot the second graph (data after counterfactual changes)
 plt.subplot(1, 2, 2)
